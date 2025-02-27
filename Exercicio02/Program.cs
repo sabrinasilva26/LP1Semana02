105664d6fc19bbbc6133f779becf1fcf29fd6ec1
sabrinasilva26 <sabrinasilva.art@gmail.com>
2025-02-27 21:06:35 +0000

    add exercise 2

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercicio02
+{
+    public class OperaInteiros
+    {
+        private static void Main(string[] args)
+        {
+            int i1 = 3;
+            int i2 = 1;
+            int i3 =−8;
+            int x = 2;
+            /// (1)= 11 
+            /// (2)= -8 
+            /// (3)= 3 
+            /// (4)= 6
+            /// (5)= 2
+            /// (6)= 12
+            /// (7)= 
+            /// (8)= 2
+            /// (9)= 4
+
+            Console . WriteLine(soma);
+        }
+    }
+}
