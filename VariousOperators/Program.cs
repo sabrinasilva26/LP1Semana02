42accbb62bfa9e98e0b98c972308e0eb7d50a691
sabrinasilva26 <sabrinasilva.art@gmail.com>
2025-02-27 22:44:01 +0000

    Add exercise 4

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número não negativo: ");
+            int input = int.Parse(Console.ReadLine());
+
+            byte n = (byte)input;
+
+            byte divisao = (byte) (n / 2);
+
+            Console.WriteLine($"{divisao}");
+
+            byte left = (byte)(n<< 3);
+
+            Console.WriteLine($"{left}");
+
+            byte x = (byte)(n ^ 6);
+
+            Console.WriteLine($"{x}");
+
+            bool s = (bool)(n > 10);
+            
+            Console.WriteLine($"{s}");
+            
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
