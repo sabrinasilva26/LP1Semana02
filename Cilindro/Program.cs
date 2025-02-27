4eed727c1c5499253915a5f7942c12dae78b0156
sabrinasilva26 <sabrinasilva.art@gmail.com>
2025-02-27 22:55:03 +0000

    Added inputs to exercise 5

---
+++ Cilindro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Indica a altura do cilindro: ");
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Indica o raio do cilindro: ");
+            int input1 = int.Parse(Console.ReadLine());
+
+            
+
+
+    
+        }
+    }
+}
