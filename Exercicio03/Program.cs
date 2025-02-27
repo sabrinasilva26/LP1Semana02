214aeae3489455230f9f07f15f194b01f5a98051
sabrinasilva26 <sabrinasilva.art@gmail.com>
2025-02-27 21:43:34 +0000

    add exercise 3

---
+++ Exercicio03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio03
+{
+    public class OperaBooleanos
+    {
+        private static void Main(string[] args)
+        {
+           float f1 = 1.4 f ;
+           float f2 =−13.7f ;
+           bool b1 = true ;
+           bool b2 = false ;
+           bool x = true ;
+           /// (1)= False
+           /// (2)= False
+           /// (3)= True
+           /// (4)= True
+           /// (5)= False
+           /// (6)= True
+           /// (7)= False
+           /// (8)= True
+           /// (9)= False 
+           x &= f2 < f1;
+           Console . WriteLine(x);
+        }
+    }
+}
