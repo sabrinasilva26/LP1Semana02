71086b333de2a054848643d1b6336d2f4b44814d
sabrinasilva26 <sabrinasilva.art@gmail.com>
2025-02-27 17:34:02 +0000

    add 1 exercise

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Indica um número inteiro: ");
+            int input = int.Parse(Console.ReadLine());
+
+            sbyte n = (sbyte)input;
+
+            sbyte incremento = --n;
+            sbyte decremento = ++n;
+
+            Console.WriteLine("Número inserido: " + n);
+
+            Console.WriteLine($"Operação de decremento de n: {incremento}");
+            Console.WriteLine($"Operação de decremento de n: {decremento}");
+
+            
+        }
+    }
+}
