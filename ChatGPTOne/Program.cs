582d6fe0facc40ba7044c6e5f799578f04f6d1c9
sabrinasilva26 <sabrinasilva.art@gmail.com>
2025-02-28 01:32:55 +0000

    Updated exercise 10

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -1,32 +1,52 @@
 ﻿using System;
 
 namespace ChatGPTOne
-{
+ {
     public class Program
     {
         private static void Main(string[] args)
         {
 
-            string = read
-            Console.WriteLine($"Insira uma questão: ");
-            read = Console.ReadLine();
+           Console.WriteLine($"Insira uma questão: ");
+            string read = Console.ReadLine();
 
-
+            while (read != "EXIT")
             {
-            if (read == "How are you?");
+            
+            if (read == "How are you?")
+            {
                 Console.WriteLine("I´m fine");
-
-            else if (read == "Can you recommend me one movie");
+            }
+            else if (read == "Can you recommend me one movie?")
+            {    
                 Console.WriteLine("I recommend you se7en");
-
-            else if ("Can you help me with my programming assignments");
-                Console.WriteLine("yes, I can help you. What do you need");
-
-            else if (read != "EXIT");
-
+            }
+            else if (read == "Can you help me with my programming assignments?")
+            {
+                Console.WriteLine("yes, I can help you. What do you need?");
             }
 
-           
+            else if (read == "How´s the weather?")
+            {
+                Console.WriteLine("It´s sunny today");
+            }
+            else
+            {
+                Console.WriteLine("I don't understand the question.");
+            }
+
+
+            Console.WriteLine($"Insira uma questão: ");
+            read = Console.ReadLine();
+            }
+
+                
+            
+            
+
         }
+
+           
     }
 }
+
