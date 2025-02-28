34d6bde104f29ac26e9be9e5bfe1eabe7b061456
sabrinasilva26 <sabrinasilva.art@gmail.com>
2025-02-28 00:40:29 +0000

    add exercise 10

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            string = read
+            Console.WriteLine($"Insira uma questão: ");
+            read = Console.ReadLine();
+
+
+            {
+            if (read == "How are you?");
+                Console.WriteLine("I´m fine");
+
+            else if (read == "Can you recommend me one movie");
+                Console.WriteLine("I recommend you se7en");
+
+            else if ("Can you help me with my programming assignments");
+                Console.WriteLine("yes, I can help you. What do you need");
+
+            else if (read != "EXIT");
+
+            }
+
+           
+        }
+    }
+}
